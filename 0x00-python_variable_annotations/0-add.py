17f4927d09715ce4090ab105395004b86be388f7
austieoduor <austineoduor57@gmail.com>
2023-01-11 21:28:39 +0000

    correction

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,6 +4,7 @@
 def add(a: float, b: float) -> float:
     '''
     type-annotated function add that takes a float a
-    and a float b as arguments and returns their sum as a float.
+    and a float b as arguments 
+    and returns their sum as a float.
     '''
-    return a+b
+    return a + b
--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-
+'''type-annotated function floor which takes a float n'''
 import math
 
 
 def floor(n: float) -> int:
-    '''type-annotated function floor which takes a float n
-    as argument and returns the floor of the float.'''
+    '''
+    type-annotated function floor which takes a float n
+    as argument and returns the floor of the float.
+    '''
     return math.floor(n)
