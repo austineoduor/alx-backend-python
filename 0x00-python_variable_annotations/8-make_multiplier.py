3f249b89f0cf11dcfd8077501f7e2eee0dc4c2dd
austieoduor <austineoduor57@gmail.com>
2023-01-11 20:49:46 +0000

    type-annotation

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''
+    a type-annotated function make_multiplier that
+    takes a float multiplier as argument
+    and returns a function that multiplies a float by multiplier
+    '''
+    def f(y):
+        return y * multiplier
+    return f
---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import List, Iterable, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''
+    Annotate the below function’s parameters and return
+    values with the appropriate types
+    '''
+    return [(i, len(i)) for i in lst]
