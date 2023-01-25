43c194ee438b52c93389a4f3aa5751d2145e172b
austieoduor <austineoduor57@gmail.com>
2023-01-25 11:26:02 +0000

    0x02. Python - Async Comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator(float, None, None):
+async def async_generator() -> Generator[float, None, None]:
     '''
     a coroutine called async_generator that takes no arguments.
     The coroutine will loop 10 times, each time asynchronously wait 1 second,
