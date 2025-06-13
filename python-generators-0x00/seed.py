1cce7195175af4750f4ed6ac21754eb30724ef13
austineoduor <austineoduor57@gmail.com>
2025-06-13 19:09:29 +0300

    mysql connections and usee_data

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -1,16 +1,112 @@
 #!/usr/bin/env python3
 import mysql.connector as mcnx
 from mysql.connector import errorcode
+
+DBNAME = "ALX_prodev"
+db_config = {'user':'root', 'password':'kali', 'database':'ALX_prodev'}
+TABLES = {}
+
+TABLES["user_data"] = ( 
+"CREATE TABLE IF NOT EXISTS `user_data`("
+"user_id INT AUTO_INCREMENT,"
+"name VARCHAR(100) NOT NULL,"
+"email VARCHAR(100) NOT NULL,"
+"age DECIMAL NOT NULL, "
+"PRIMARY KEY (`user_id`)"
+
+")ENGINE=InnoDB")
+
 def connect_db():
+    '''
+    creates a connection to the mysqlserver
+    and retuns a connection
+    '''
     try:
         cnx = mcnx.connect(user='root', password='kali')
-        print("successful")
-        return cnx
+        if cnx.is_connected():
+            print("successful connected to mysql-sever")
+            return cnx
     except mcnx.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your usernmae or password")
+            exit(1)
         else:
             print(err)
+            exit(1)
+
+
+def create_database(connection):
+    '''
+    creates the database if it doesn't exist,
+    and returns a cursor
+    '''
+    cursor = connection.cursor()
+    try:
+        dbnx = cursor.execute("CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARACTER SET utf8;".format(DBNAME))
+        print("connected to mysql server and created ALX_prodev database")
+        return dbnx
+    except mcnx.Error as err:
+        print(err)
+        exit(1)
+
+
+def connect_to_prodev():
+    '''
+    creates connection to database ALX_prodev
+    '''
+    try :
+        cnx = mcnx.connect(**db_config)
+        if cnx.is_connected():
+            print("connected to ALX_prodev")
+            return cnx
+    except mcnx.Error as err:
+        '''if err.errno == errorcode.ER.BAD_DB_ERROR:'''
+        print("The requested database is: {}".format(err))
+        exit(1)
+
+
+def create_table(connection):
+    '''
+    creates table user_data, if the table doesn't exist
+    '''
+    if connection.is_connected():
+        cursor = connection.cursor()
+        for table in TABLES:
+            table_description = TABLES[table]
+            #cursor.execute("USE {};".format(DBNAME))
+            #print("Using: {}".format(DBNAME))
+            try:
+                cursor.execute(table_description)
+                print("created the table")
+            except mcnx.Error as err:
+                if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                    print("Already present {}".format(err))
+                    exit(1)
+                else:
+                    print(err)
+    else:
+        print("No database connection")
+        exit(1)
+
+
+def insert_data(connection, data):
+    ''' insert data to the ALX_prodev database in user_data table'''
+
+    user_data = "user_data.csv"
+    with user_adata as data:
+
+
+    '''if connection.is_connected():
+        cursor = connection.cursor()
+        try:
+    else:
+        print("No connection")
+        exit(1)'''
+
+
 
 if __name__ == "__main__":
-    connect_db()
+    cnn = connect_db()
+    create_database(cnn)
+    con = connect_to_prodev()
+    create_table(con)
