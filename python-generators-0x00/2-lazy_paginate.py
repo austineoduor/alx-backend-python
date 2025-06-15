69c0689d72be102677be4f75ada3c62a98d2e36b
austineoduor <austineoduor57@gmail.com>
2025-06-15 13:46:55 +0300

    lazy pagination with offset and limit

---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+seed = __import__('seed')
+
+
+def paginate_users(page_size, offset):
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
+    
+    
+def lazy_paginate(page_size):
+    '''retrieves rows from from offset to Limit'''
+    offset = 0
+        while True:
+            data = paginate_users(page_size, offset)
+            if data:
+                offset += page_size
+                for row in data:
+                    yield row
+            else:
+                print("data found")
+                exit(1)
