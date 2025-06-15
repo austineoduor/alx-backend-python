31098058eed999b8d5a9022d5fad6cf174ee8fff
austineoduor <austineoduor57@gmail.com>
2025-06-15 16:11:58 +0300

    average age

--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -7,14 +7,16 @@ def stream_user_ages():
     print("calling ...")
     page_size = 50
     offset = 0
-    data = user_data.paginate_users(page_size, offset)
-    if data:
+    while True:
+        data = user_data.paginate_users(page_size, offset)
+        if data:
 
-        for user in data:
-            print(user['age'])
-            yield user('age')
-    else:
-        print("No data found")
+            for user in data:
+                print(user['age'])
+                yield user('age')
+        else:
+            print("No data found")
+            exit(1)
     
 def calculating_average_age():
     while True:
