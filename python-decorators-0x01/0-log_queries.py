7cbebf0b8cb3b56b907e10830c1499c282d4bde6
austineoduor <austineoduor57@gmail.com>
2025-06-24 00:52:05 +0300

    dectorator query logger

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -5,18 +5,19 @@ import functools
 import logging
 #### decorator to log SQL queries
 
- def log_queries():
-     functools.wrap()
-     logger = logging.getLogger(__name__)
-     hsndler = logging.formatter(query.log)
-     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s : %(message)s")
-     handler.setFormatter(formatter)
-     logger.addHandler(handler)
+def log_queries(func):
+    functools.wraps(func)
+    logger = logging.getLogger(__name__)
+    handler = logging.FileHandler('query.log')
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s : %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
      
-     def log_queries_wrapper(*args, **kwargs):
-         query = f"{}{}".format(*args, **kwargs)
-         logger.info(query)
-         return log_queries_wrapper
+    def log_queries_wrapper(*args, **kwargs):
+        query = "Executing: {} - {}".format(*args, **kwargs)
+        logger.info(query)
+        return func(*args,**kwargs)
+        return log_queries_wrapper
          
          
 
