8b15f123a29340bff95f272738400b271a8a4762
austineoduor <austineoduor57@gmail.com>
2025-06-24 02:22:20 +0300

    openning db connection and closing it in decorator wrapper function

--- python-decorators-0x01/1-with_db_connection.py
+++ python-decorators-0x01/1-with_db_connection.py
@@ -5,7 +5,21 @@ import sqlite3
 import functools
 
 def with_db_connection(func):
-    """ your code goes here""" 
+    
+    @functools.wraps(func)  
+    def wrapper(*args, **kwargs):
+        conn = None  # Initialize conn outside the try block for scope
+        try:
+            conn = sqlite3.connect('users.db')
+            result = func(conn, *args, **kwargs)
+            return result
+        except sqlite3.Error as e:
+            print(f"Database error: {e}")
+            return None  # Or raise the exception
+        finally:
+            if conn:
+                conn.close()
+    return wrapper
 
 @with_db_connection 
 def get_user_by_id(conn, user_id): 
