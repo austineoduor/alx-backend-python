03e6368bfddf1a7b7676c51a2f157da2c9abfe66
austineoduor <austineoduor57@gmail.com>
2025-06-24 03:19:32 +0300

    openning db connection and closing it in decorator wrapper function

--- python-decorators-0x01/4-cache_query.py
+++ python-decorators-0x01/4-cache_query.py
@@ -7,7 +7,37 @@ import functools
 
 query_cache = {}
 
-"""your code goes here"""
+def cache_query(func):
+    """
+    Decorator to cache the results of a query based on the query string.
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        query = kwargs.get('query')
+        if query in query_cache:
+            ####print("Fetching from cache...")
+            return query_cache[query]
+        else:
+            ####print("Executing query and caching...")
+            result = func(*args, **kwargs)
+            query_cache[query] = result
+            return result
+    return wrapper
+
+
+def with_db_connection(func):
+    """
+    Decorator to manage the database connection.
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            result = func(conn, *args, **kwargs)
+            return result
+        finally:
+            conn.close()
+    return wrapper
 
 @with_db_connection
 @cache_query
@@ -18,6 +48,8 @@ def fetch_users_with_cache(conn, query):
 
 #### First call will cache the result
 users = fetch_users_with_cache(query="SELECT * FROM users")
+print(users)
 
 #### Second call will use the cached result
 users_again = fetch_users_with_cache(query="SELECT * FROM users")
+print(users_again)
