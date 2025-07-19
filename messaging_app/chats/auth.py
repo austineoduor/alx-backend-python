40c614cc4dc99d11f2c609cd7b8c1086013111a4
austineoduor <austineoduor57@gmail.com>
2025-07-19 23:35:13 +0300

    update

---
+++ messaging_app/chats/auth.py
@@ -0,0 +1,14 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
+class customTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['username'] = user.username 
+        token['email'] = user.email
+        token['user_id'] = str(user.user_id)
+        return token
